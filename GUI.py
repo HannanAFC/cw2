d772122db3b029521e871448a1b23000e71082f4
M4TTH3W154 <matthewpotter154@gmail.com>
2024-12-04 21:22:22 +0000

    Update GUI.py
    Can now take parameters for the calculations from the sign up information.

--- GUI.py
+++ GUI.py
@@ -18,6 +18,8 @@ class TakeHomeGUI:
         self.create_label_and_entry("Debt")
         self.create_label_and_textbox("Additional Information")
 
+        sign_up_button = Button(self.root, text="Sign Up", command=self.Sign_Up)
+        sign_up_button.pack()
     def create_label_and_entry(self, text):
         label = Label(self.root, text=text)
         label.pack()
@@ -40,11 +42,18 @@ class TakeHomeGUI:
         setattr(self, f"{text.lower().replace(' ', '_')}Textbox", textbox)
 
     def Sign_Up(self):
-        pass
-    
+        Username = self.usernameEntry.get()
+        Password = self.passwordEntry.get()
+        Annual_Gross_Income = self.annual_gross_incomeCombobox.get()
+        Employment_Status = self.employment_statusCombobox.get()
+        Debt = self.debtEntry.get()
+        Additional_Information = self.additional_informationTextbox.get("1.0", "end-1c")
+        
+
 
 
 if __name__ == "__main__":
     root = Tk()
     app = TakeHomeGUI(root)
     root.mainloop()
+    
