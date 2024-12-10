b9305f17b94e972ca2c676c18d9d2e45ba6124b2
hannan <hannanafc18@gmail.com>
2024-12-10 14:47:39 +0000

    Added all plan types for national insurance

--- calculations.py
+++ calculations.py
@@ -27,11 +27,86 @@ class Income:
 
     def returnNationalInsurance(self, plan):
         #for now have done it for plan A
-        #am using nested dict to allow us to store all the brackets and plans in one place
+        #I am using nested dict to allow us to store all the brackets and plans in one place
         planList = {
             "a": {
             "contribution1": 0,
             "contribution2": 0.08,
+            "contribution3": 0.02
+            },
+            "b": {
+            "contribution1": 0,
+            "contribution2": 0.0185,
+            "contribution3": 0.02
+            },
+            "c": {
+            "contribution1": 0,
+            "contribution2": 0,
+            "contribution3": 0
+            },
+            "d": {
+            "contribution1": 0,
+            "contribution2": 0.02,
+            "contribution3": 0.02
+            },
+            "e": {
+            "contribution1": 0,
+            "contribution2": 0.0185,
+            "contribution3": 0.02
+            },
+            "f": {
+            "contribution1": 0,
+            "contribution2": 0.08,
+            "contribution3": 0.02
+            },
+            "h": {
+            "contribution1": 0,
+            "contribution2": 0.08,
+            "contribution3": 0.02
+            },
+            "i": {
+            "contribution1": 0,
+            "contribution2": 0.0185,
+            "contribution3": 0.02
+            },
+            "j": {
+            "contribution1": 0,
+            "contribution2": 0.02,
+            "contribution3": 0.02
+            },
+            "k": {
+            "contribution1": 0,
+            "contribution2": 0,
+            "contribution3": 0
+            },
+            "l": {
+            "contribution1": 0,
+            "contribution2": 0.02,
+            "contribution3": 0.02
+            },
+            "m": {
+            "contribution1": 0,
+            "contribution2": 0.08,
+            "contribution3": 0.02
+            },
+            "n": {
+            "contribution1": 0,
+            "contribution2": 0.08,
+            "contribution3": 0.02
+            },
+            "s": {
+            "contribution1": 0,
+            "contribution2": 0,
+            "contribution3": 0
+            },
+            "v": {
+            "contribution1": 0,
+            "contribution2": 0.08,
+            "contribution3": 0.02
+            },
+            "z": {
+            "contribution1": 0,
+            "contribution2": 0.02,
             "contribution3": 0.02
             }
         }
@@ -39,7 +114,7 @@ class Income:
         #only the percentage contribution per threshold changes not the threshold itself
         #so we can use the threshold as the conditions and have the tax amount be taken from the dict
         if self.monthly <= 1048:
-            contribution = planList[(plan)]["contribution1"] * 1048
+            contribution = planList[plan]["contribution1"] * 1048
             return contribution
         elif self.monthly > 1048 and self.monthly <= 4189:
             bracket1 = planList[plan]["contribution1"] * 1048
