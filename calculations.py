ef81a63f1c60a6284920c8a85d307276e54b96f8
hannan <hannanafc18@gmail.com>
2024-12-16 11:42:49 +0000

    Added validation for NI plan list, made it a private variable for each object.

--- calculations.py
+++ calculations.py
@@ -2,10 +2,11 @@
 #parameters will probably change when we add student loans
 #Hannan - Income tax calculator made on 5/12/24, National Insurance 10/12/24
 class Income:
-    def __init__(self, name, income, monthly):
+    def __init__(self, name, income, monthly, plan):
         self.name = name
         self.income = income
         self.monthly = monthly
+        self.plan = plan
 
     @property
     def name(self):
@@ -42,29 +43,41 @@ class Income:
         self.validateIncome(monthly)
         self.__monthly = monthly
 
+    @property
+    def plan(self):
+        return self.__plan
+
+    @plan.setter
+    def plan(self, plan):
+        self.validatePlan(plan)
+        self.__plan = plan
+
+    def validatePlan(self, plan):
+        if plan not in ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 's', 'v', 'z']:
+            raise ValueError("Invalid plan")
 
 
     #def validateIncome(self, value):
 
     #method calculates income income after tax and returns
     def returnIncomeAfterTax(self):
-        if self.income <= 12570:
-            return self.income
-        elif self.income <= 50270:
-            return 12570 + ((self.income - 12570)*0.8)
-        elif self.income <= 125140:
+        if self.__income <= 12570:
+            return self.__income
+        elif self.__income <= 50270:
+            return 12570 + ((self.__income - 12570)*0.8)
+        elif self.__income <= 125140:
             bracket1 = 12570
             bracket2 = (50270-12570)*0.8
-            bracket3 = (self.income - 50270)*0.6
+            bracket3 = (self.__income - 50270)*0.6
             return bracket1 + bracket2 + bracket3
-        elif self.income > 125140:
+        elif self.__income > 125140:
             bracket1 = 12570
             bracket2 = (50270-12570)*0.8
             bracket3 = (125140 - 50270)*0.6
-            bracket4 = (self.income - 125140)*0.55
+            bracket4 = (self.__income - 125140)*0.55
             return bracket1 + bracket2 + bracket3 + bracket4
 
-    def returnNationalInsurance(self, plan):
+    def returnNationalInsurance(self):
         #for now have done it for plan A
         #I am using nested dict to allow us to store all the brackets and plans in one place
         #the code below it takes the inputted plan that the user gives, finds it in the dictionary and uses its associated
@@ -154,22 +167,22 @@ class Income:
 
         #only the percentage contribution per threshold changes not the threshold itself
         #so we can use the threshold as the conditions and have the tax amount be taken from the dict
-        if self.monthly <= 1048:
-            contribution = planList[plan]["contribution1"] * 1048
+        if self.__monthly <= 1048:
+            contribution = planList[self.__plan]["contribution1"] * 1048
             return contribution
-        elif self.monthly > 1048 and self.monthly <= 4189:
-            bracket1 = planList[plan]["contribution1"] * 1048
-            bracket2 = planList[plan]["contribution2"] * (self.monthly - 1048)
+        elif self.__monthly > 1048 and self.__monthly <= 4189:
+            bracket1 = planList[self.__plan]["contribution1"] * 1048
+            bracket2 = planList[self.__plan]["contribution2"] * (self.monthly - 1048)
             return bracket1 + bracket2
-        elif self.monthly > 4189:
-            bracket1 = planList[plan]["contribution1"] * 1048
-            bracket2 = planList[plan]["contribution2"] * (4189 - 1048)
-            bracket3 = planList[plan]["contribution3"] * (self.monthly - 4189)
+        elif self.__monthly > 4189:
+            bracket1 = planList[self.__plan]["contribution1"] * 1048
+            bracket2 = planList[self.__plan]["contribution2"] * (4189 - 1048)
+            bracket3 = planList[self.__plan]["contribution3"] * (self.monthly - 4189)
             return bracket1 + bracket2 + bracket3
 
     #def returnStudentLoans(self):
 
-John = Income('John', 36790, 36790/12)
+John = Income('John', 36790, 36790/12, "a")
 print(John.returnIncomeAfterTax())
-print(John.returnIncomeAfterTax() - John.returnNationalInsurance("a") * 12)
+print(John.returnIncomeAfterTax() - John.returnNationalInsurance() * 12)
 #getting take home pay of 30,008 from MSE calculator and 30008.88 from this so must be right.
